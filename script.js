6fe3cda6220d67ca21e2bbfeec47caaceb36c003
Bulletukk <benny@tuktuk.no>
2024-11-25 21:43:41 +0000

    Snake moves!

--- script.js
+++ script.js
@@ -1,34 +1,89 @@
-//let x = 6;
-var snakeLength = 3;
-var xPos = 7;
-var yPos = 4;
-var xDir = 1;
-var yDir = 0;
-var vel = 1.1;
-var snakeMoveInterval = 500;
+class Snake {
+    constructor() {
+        this.snakeLength = 3;
+        this.xDir = 1;
+        this.yDir = 0;
+        this.positions = [[5,6],[4,6],[3,6]];
+        this.shouldLengthen = false;
+        this.skipFrequency = 4; //How many frame updates between each snake move.
+    }
+
+    setDir(key) {
+        if (key=="ArrowUp" && this.yDir!=1){
+            this.xDir = 0;
+            this.yDir = -1;
+            return true;
+        } else if (key=="ArrowLeft" && this.xDir!=1){
+            this.xDir = -1;
+            this.yDir = 0;
+            return true;
+        } else if (key=="ArrowDown" && this.yDir!=-1){
+            this.xDir = 0;
+            this.yDir = 1;
+            return true;
+        } else if (key=="ArrowRight" && this.xDir!=-1){
+            this.xDir = 1;
+            this.yDir = 0;
+            return true;
+        }
+        return false;
+    }
+    move() {
+        if (this.shouldLengthen==true){
+            this.positions.push(this.positions[this.positions.length-1])
+            for (var i=this.positions.length-2;i--;i>0){
+                this.positions[i] = this.positions[i-1];
+            }
+            this.positions[0][0] += this.xDir;
+            this.positions[0][1] += this.yDir;
+            this.shouldLengthen=false;
+            if (this.skipFrequency>1){
+                this.skipFrequency -= 0.25;
+            }
+        } else {
+            for (var i=this.positions.length-1;i>0;i--){
+                this.positions[i] = [this.positions[i-1][0],this.positions[i-1][1]];
+            }
+            this.positions[0][0] += this.xDir;
+            this.positions[0][1] += this.yDir;
+        }
+    }
+};
+//Method: multiply snakemoveinterval by 4/5 every time.
+var s = new Snake();
+var minSnakeMoveInterval = 50;
+var recWidth = 620;
+var recHeight = 300;
+var takeInput = false;
+var updateCount = 0;
 
 const canvas = document.getElementById("snakeGame");
 const ctx = canvas.getContext("2d");
 ctx.fillStyle = "white";
-ctx.fillRect(0, 0, 300, 200);
-ctx.fillStyle = "black";
-ctx.fillRect(20*xPos, 20*yPos, 20, 20);
+ctx.fillRect(0, 0, recWidth, recHeight);
 
 window.addEventListener("keydown", (e) => {
-    if (e.code=="ArrowUp"){
-        xDir = 0;
-        yDir = -1;
+    if (takeInput==true){
+        s.setDir(e.code);
+        takeInput=false;
     }
-  });
+});
 
 snakeInterval = setInterval(() => {
-    time = 0;
-    ctx.clearRect(0,0,300,200);
-    xPos += xDir*vel;
-    yPos += yDir*vel;
+    ctx.clearRect(0,0,recWidth,recHeight);
     ctx.fillStyle = "white";
-    ctx.fillRect(0, 0, 300, 200);
+    ctx.fillRect(0, 0, recWidth, recHeight);
     ctx.fillStyle = "black";
-    ctx.fillRect(20*xPos, 20*yPos, 20, 20);
-}, snakeMoveInterval);
+    for(var i=0;i<s.positions.length;i++){
+        console.log(20*s.positions[i][0],20*s.positions[i][1]);
+        ctx.fillRect(20*s.positions[i][0], 20*s.positions[i][1], 20, 20);
+    }
+    if (updateCount>s.skipFrequency){
+        s.move();
+        updateCount = 0;
+        takeInput = true;
+    } else {
+        updateCount++;
+    }
+}, minSnakeMoveInterval);
 //To stop this function, call clearInterval(snakeInterval);
